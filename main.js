acea1797f348360c8f2797f3eed684c52981ab83
1uday576 <126719344+1uday576@users.noreply.github.com>
2024-01-05 18:59:23 -0500

    sorted fix

--- main.js
+++ main.js
@@ -15,23 +15,17 @@ function sort(){
     let array = tmp.map(function(str){
         return parseInt(str);
     });
-    console.log(array)
     
     for(let i = 0; i < array.length; i++){
         for(let j = 0; j < array.length - 1; j++){
             if(array[j] > array[j+1]){
-                swap(j, j + 1);
+                let tmpA = array[j];
+                let tmpB = array[j+1];
+                array[j] = tmpB;
+                array[j+1] = tmpA;
             }
         }
     }
 
     console.log(String(array))
-}
-
-function swap(a, b){
-    let tmpA = array[a];
-    let tmpB = array[b];
-
-    array[a] = tmpB;
-    array[b] = tmpA;
 }
