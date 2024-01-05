0718b6e147802fa77111168cf7d9fbf22e062a26
1uday576 <126719344+1uday576@users.noreply.github.com>
2024-01-05 18:40:09 -0500

    sorting

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function sort(){
     for(let i = 0; i < array.length; i++){
         for(let j = 0; j < array.length - 1; j++){
             if(array[j] > array[j+1]){
-                swap(j, j -1);
+                swap(j, j + 1);
             }
         }
     }
