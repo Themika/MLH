d5769e3c2a7528c34ae1d483a3a9f6c363318bd0
Hamza-Alajeely <hamzaalajeely08@gmail.com>
2024-01-06 17:23:46 -0500

    NASA API

---
+++ Nasa API/main2.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk 
+from tkinter import *
+from tkinter import font
+
+win = tk.Tk()
+win.title("NASA API")
+win.configure(bg='black')
+win.geometry('1920x1080')
+
+
+win.mainloop()
