e405ede533e4633cfe88df1ce416d9052cfa3da5
Hamza-Alajeely <hamzaalajeely08@gmail.com>
2024-01-06 19:20:11 -0500

    UI

--- Nasa API/main2.py
+++ Nasa API/main2.py
@@ -5,13 +5,27 @@ from tkinter import font
 from PIL import Image, ImageTk
 from tkinter import filedialog
 
+
 win = tk.Tk()
 win.title("NASA API")
-win.configure(bg='black')
+
 win.geometry('1920x1080')
 
-title = ttk.Label(win, text='Asteroid Tracking', font=('Arial Black', 40, 'bold'), background='black', foreground='white')
-
+title = ttk.Label(text='Asteroid Search', font=('Arial Black', 40, 'bold'), background='black', foreground='white')
 title.place(relx = 0.5, rely = 0, anchor = N)
+
+des = ttk.Label (text='Enter a start and end date to see what asteroids were close to earth during that period', font=('Arial', 15), background='black', foreground='white')
+des.place(relx = 0.5, rely = 0.09, anchor= CENTER)
+
+
+
+
+
+
+
+
+
+
+
 
 win.mainloop()
